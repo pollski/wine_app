c7d9340897f4cadd8091ff7b25c8d8f64968fa57
Ryan Poll <ryanpoll@gmail.com>
2013-06-20 16:06:21 -0700

    Finsih sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ SampleApp::Application.routes.draw do
  	match '/about', to: 'static_pages#about'
  	match '/contact', to: 'static_pages#contact'
 	match '/signup', to: 'users#new'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  	match '/signup', to: 'users#new'
+  	match '/signin', to: 'sessions#new'
+  	match '/signout', to: 'sessions#destroy',  via: :delete
   
 #  root to: 'static_pages#home'
   
